6e528d49b8b0093c2091f25540ef7b21a36ffbfa
yazici2516 <yazicimurat575@gmail.com>
2025-12-26 04:18:22 +0300

    Tablo olusturma garantisi eklendi

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using KutuphaneYonetim.Data;
 using Microsoft.EntityFrameworkCore;
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
 
